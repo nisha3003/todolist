f0936989722a5a885525f9efa828e0df7d7ccec1
nisha3003 <51108048+nisha3003@users.noreply.github.com>
2021-12-26 09:01:18 +0530

    Add files via upload

---
+++ build/precache-manifest.0741918b6da1055e0c07a557e488e4b7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4023156dcec4a8b3f4c5b679b3cf46c",
+    "url": "/To-Do-App/index.html"
+  },
+  {
+    "revision": "e19885bd7d0b527037fd",
+    "url": "/To-Do-App/static/css/main.e26b94d0.chunk.css"
+  },
+  {
+    "revision": "c22ce226bc9b079ee3d5",
+    "url": "/To-Do-App/static/js/2.b0ebaed3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/To-Do-App/static/js/2.b0ebaed3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e19885bd7d0b527037fd",
+    "url": "/To-Do-App/static/js/main.7e52ea41.chunk.js"
+  },
+  {
+    "revision": "cc4f0bc861a97723a984",
+    "url": "/To-Do-App/static/js/runtime-main.baa0f9d7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/To-Do-App/static/media/logo.5d5d9eef.svg"
+  }
+]);
